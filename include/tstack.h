779d2779006ee482ec22aa2cb53aa55d26417210
kodt2 <kodt2005@yandex.ru>
2024-11-15 09:45:20 +0300

    structure

---
+++ include/tstack.h
@@ -0,0 +1,295 @@
+﻿#include <iostream>
+template<typename T>
+class Tlist {
+private:
+	struct Node {
+		T data;
+		Node* next;
+	};
+	class iterator {
+	public:
+		Node* node;
+		iterator() {
+			node = nullptr;
+		}
+		iterator(Node* n) {
+			node = n;
+		}
+		Node& operator*() {
+			return *node;
+		}
+		Node operator*() const {
+			return *node;
+		}
+		iterator& operator++() {
+			node = node->next;
+			return *this;
+		}
+		bool operator==(iterator iter) {
+			return node == iter.node;
+		}
+		bool operator!=(iterator iter) {
+			return node != iter.node;
+		}
+	};
+	Node* first;
+public:
+	Tlist() {
+		first = nullptr;
+	};
+	Tlist(int n, T deflt = T()) {
+		if (n <= 0) {
+			throw "eogts";
+		}
+		Node* temp = new Node();
+		temp->data = deflt;
+		temp->next = nullptr;
+		first = temp;
+		Node* current = first;
+		for (int i = 1; i < n; i++) {
+			temp = new Node();
+			temp->data = deflt;
+			temp->next = nullptr;
+			current->next = temp;
+			current = temp;
+		}
+	}
+	~Tlist() {
+		Node* current = first;
+		Node* temp;
+		while (current!=nullptr) {
+			temp = current;
+			current = current->next;
+			delete temp;
+		}
+		first = nullptr;
+	};
+	Tlist(const Tlist& list) {
+		Node* current_old = list.first;
+		Node* temp = new Node();
+		temp->data = current_old->data;
+		temp->next = nullptr;
+		first = temp;
+		Node* current_new = first;
+		current_old = current_old->next;
+		while (current_old!=nullptr) {
+			temp = new Node();
+			temp->data = current_old->data;
+			temp->next = nullptr;
+			current_new->next = temp;
+			current_new = temp;
+			current_old = current_old->next;
+		}
+	};
+	bool operator==(const Tlist& list) const {
+		bool res = true;
+		Node* current2 = list.first;
+		Node* current1 = first;
+		while (current1 != nullptr || current2 != nullptr) {
+			if ((current1 == nullptr && current2 != nullptr) || (current2 == nullptr && current1 != nullptr)) {
+				res = false;
+				return res;
+			}
+			if (current1->data != current2->data) {
+				res = false;
+				return res;
+			}
+			current1 = current1->next;
+			current2 = current2->next;
+		}
+		return res;
+	}
+	Tlist& operator= (const Tlist& list) {
+		if (first == list.first) {
+			return *this;
+		}
+		Tlist* res = new Tlist(list);
+		Tlist* temp = new Tlist();
+		temp->first = first;
+		delete temp;
+		first = res->first;
+		res->first = nullptr;
+		delete res;
+		return *this;
+	}
+	Tlist& replace(Tlist& list) {
+		if (first == list.first) {
+			return *this;
+		}
+		Tlist* temp = new Tlist();
+		temp->first = first;
+		delete temp;
+		first = list.first;
+		return *this;
+	}
+	void print() {
+		Node* current = first;
+		while (current != nullptr) {
+			std::cout << current->data << " ";
+			current = current->next;
+		}
+		std::cout << "\n";
+	};
+	T& operator[](int index) {
+		if (index<0 || index>=this->size()) {
+			throw "list index out of range";
+		}
+		Node* current_ptr = first;
+		int cur_ind = 0;
+		T res = current_ptr->data;
+		while (current_ptr != nullptr && cur_ind < index) {
+			current_ptr = current_ptr->next;
+			cur_ind++;
+			res = current_ptr->data;
+		}
+		if (current_ptr == nullptr) {
+			throw "no such element";
+		}
+		return res;
+
+	};//O(n)
+	void set_element(T value, int index) {
+		iterator t;
+		try {
+			t = find_ptr(index);
+		}
+		catch (...) {
+			throw  "list index out of range";
+		}
+		t.node->data = value;
+	}
+	iterator insert(T value, iterator prev) {
+		Node* temp = new Node();
+		temp->data = value;
+		temp->next = prev.node->next;
+		prev.node->next = temp;
+		return iterator(temp);
+	};
+	iterator insert_front(T value) {
+		Node* temp = new Node();
+		temp->data = value;
+		temp->next = first;
+		first = temp;
+		return iterator(first);
+	};
+	iterator errase(iterator prev) {
+		Node* temp = new Node();
+		temp = prev.node->next;
+		if (temp) {
+			prev.node->next = temp->next;
+			delete temp;
+		}
+		return first;
+	};
+	iterator errase_front() {
+		Node* temp = first;
+		first = first->next;
+		delete temp;
+		return iterator(first);
+	};
+	iterator find(T value) {
+		Node* current = first;
+		Node* res = nullptr;
+		while (current != nullptr) {
+			if (current->data == value) {
+				res = current;
+			}
+			current = current->next;
+		}
+		return iterator(res);
+	};//O(n)
+	iterator find_ptr(int index) {
+		if (index<0 || index>=this->size()) {
+			throw "list index out of range";
+		}
+		Node* current = first;
+		for(int i=0; i<index;i++) {
+			current = current->next;
+		}
+		return iterator(current);
+	}
+	size_t size() {
+		size_t i = 0;
+		Node* current = first;
+		while (current != nullptr) {
+			//std::cout << current->data << " ";
+			current = current->next;
+			i++;
+		}
+		return i;
+	};
+
+	Tlist& merge_sorted_lists(Tlist& list) {
+		if (list.first == nullptr) {
+			Tlist* res = new Tlist(*this);
+			return *res;
+		}
+		if (first == nullptr) {
+			Tlist* res = new Tlist(list);
+			return *res;
+		}
+		Node* curr1 = first;
+		Node* curr2 = list.first;
+
+		Tlist* res = new Tlist();
+		Node* curr;
+		if (curr1->data < curr2->data)
+		{
+			Node* temp = new Node();
+			temp->data = curr1->data;
+			res->first = temp;
+			curr = temp;
+			curr1 = curr1->next;
+		}
+		else
+		{
+			Node* temp = new Node();
+			temp->data = curr2->data;
+			res->first = temp;
+			curr = temp;
+			curr2 = curr2->next;
+		}
+		while (curr1 != nullptr && curr2 != nullptr)
+		{
+			if (curr1->data < curr2->data)
+			{
+				Node* temp = new Node();
+				temp->data = curr1->data;
+				curr->next = temp;
+				curr = temp;
+				curr1 = curr1->next;
+			}
+			else
+			{
+				Node* temp = new Node();
+				temp->data = curr2->data;
+				curr->next = temp;
+				curr = temp;
+				curr2 = curr2->next;
+			}
+		}
+		while (curr1 != nullptr)
+		{
+			Node* temp = new Node();
+			temp->data = curr1->data;
+			curr->next = temp;
+			curr = temp;
+			curr1 = curr1->next;
+		}
+		while (curr2 != nullptr)
+		{
+			Node* temp = new Node();
+			temp->data = curr2->data;
+			curr->next = temp;
+			curr = temp;
+			curr2 = curr2->next;
+		}
+		return *res;
+	}
+	iterator begin() {
+		return iterator(first);
+	}
+	iterator end() {
+		return iterator(nullptr);
+	}
+};
---
+++ test/test_stack.cpp
@@ -0,0 +1,190 @@
+#include "tlist.h"
+
+#include <gtest.h>
+
+TEST(Tlist, can_create_list_with_positive_length)
+{
+	ASSERT_NO_THROW(Tlist<int> m(5));
+}
+
+TEST(Tlist, throws_when_create_list_with_negative_length)
+{
+	ASSERT_ANY_THROW(Tlist<int> m(-5));
+}
+
+TEST(Tlist, can_create_copied_list)
+{
+	Tlist<int> m(5);
+	ASSERT_NO_THROW(Tlist<int> m1(m));
+}
+
+TEST(Tlist, copied_list_is_equal_to_source_one)
+{
+	Tlist<int> m(5);
+	Tlist<int> m1(m);
+	EXPECT_EQ(m, m1);
+}
+
+TEST(Tlist, copied_list_has_its_own_memory)
+{
+	Tlist<int> m(5);
+	Tlist<int> m1(m);
+	m1[1] = 1;
+	Tlist<int> res(5);
+	res[1] = 1;
+	EXPECT_EQ(m1, res);
+}
+
+TEST(Tlist, can_get_size)
+{
+	int sz = 5;
+	Tlist<int> l(sz);
+	EXPECT_EQ(sz, l.size());
+}
+
+TEST(Tlist, can_get_element)
+{
+	Tlist<int> m(5);
+	ASSERT_NO_THROW(m[3]);
+}
+
+TEST(Tlist, throws_when_get_element_with_negative_index)
+{
+	Tlist<int> m(5);
+	ASSERT_ANY_THROW( m[-1]);
+}
+
+TEST(Tlist, throws_when_get_element_with_too_large_index)
+{
+	Tlist<int> m(5);
+	ASSERT_ANY_THROW(m[6]);
+}
+
+TEST(Tlist, can_assign_matrix_to_itself)
+{
+	Tlist<int> m(5);
+	ASSERT_NO_THROW(m = m);
+	EXPECT_EQ(m,m);
+}
+
+TEST(Tlist, can_assign_matrices_of_equal_size)
+{
+	Tlist<int> m1(5), m2(5);
+	ASSERT_NO_THROW(m1 = m2);
+	EXPECT_EQ(m1,m2);
+}
+
+TEST(Tlist, assign_operator_change_matrix_size)
+{
+	Tlist<int> m1(5), m2(3);
+	m1 = m2;
+	EXPECT_EQ(m1.size(), 3);
+}
+
+TEST(Tlist, can_assign_matrices_of_different_size)
+{
+	Tlist<int> m1(5), m2(3);
+	ASSERT_NO_THROW(m1 = m2);
+	EXPECT_EQ(m1, m2);
+}
+
+TEST(Tlist, compare_equal_matrices_return_true)
+{
+	Tlist<int> m1(5), m2(5);
+	m1[0] = 100;
+	m2[0] = 100;
+	EXPECT_EQ(true, m1==m2);
+}
+
+TEST(Tlist, compare_matrix_with_itself_return_true)
+{
+	Tlist<int> m1(5);
+	EXPECT_EQ(true, m1 == m1);
+}
+
+TEST(Tlist, matrices_with_different_size_are_not_equal)
+{
+	Tlist<int> m1(5), m2(3);
+	EXPECT_EQ(false, m1 == m2);
+}
+
+TEST(Tlist, can_insert_element)
+{
+	Tlist<int> m1(3);
+	Tlist<int> res(4);
+	res.set_element(100, 3);
+	m1.insert(100, m1.find_ptr(2));
+	EXPECT_EQ(true, m1==res);
+}
+
+TEST(Tlist, can_insert_front)
+{
+	Tlist<int> m1(3);
+	Tlist<int> res(4);
+	res.set_element(100, 0);
+	m1.insert_front(100);
+	EXPECT_EQ(res, m1);
+}
+
+TEST(Tlist, can_erraze_element)
+{
+	Tlist<int> m1(3);
+	Tlist<int> res(2);
+	m1.errase(m1.find_ptr(1));
+	EXPECT_EQ(res, m1);
+}
+
+TEST(Tlist, can_erraze_front)
+{
+	Tlist<int> m1(3);
+	Tlist<int> res(2);
+	m1.errase_front();
+	EXPECT_EQ(res, m1);
+}
+
+TEST(Tlist, can_merge_sorted_lists_and_output_list_also_sorted)
+{
+	int sz1 = 5, sz2 = 2;
+	Tlist<int> m1(sz1), m2(sz2), res(sz1 + sz2), m3(sz1+sz2);
+	m1[0] = 0;
+	m1[1] = 2;
+	m1[2] = 3;
+	m1[3] = 5;
+	m1[4] = 10000;
+	m2[0] = 1;
+	m2[1] = 100000;
+	res[0] = 0;
+	res[1] = 1;
+	res[2] = 2;
+	res[3] = 3;
+	res[4] = 5;
+	res[5] = 10000;
+	res[6] = 100000;
+	m3.replace(m1.merge_sorted_lists(m2));
+	EXPECT_EQ(m3, res);
+}
+TEST(Tlist, can_merge_list_and_null_list)
+{
+	int sz1 = 5;
+	Tlist<int> m1(sz1), m2, res(sz1), m3;
+	m1[0] = 0;
+	m1[1] = 2;
+	m1[2] = 3;
+	m1[3] = 5;
+	m1[4] = 10000;
+	res[0] = 0;
+	res[1] = 2;
+	res[2] = 3;
+	res[3] = 5;
+	res[4] = 10000;
+	m3.replace(m1.merge_sorted_lists(m2));
+	EXPECT_EQ(m3, res);
+}
+TEST(Tlist, can_get_begin_iterator) {
+	Tlist<float> l;
+	ASSERT_NO_THROW(l.begin());
+}
+TEST(Tlist, can_get_end_iterator) {
+	Tlist<float> l;
+	ASSERT_NO_THROW(l.end());
+}
